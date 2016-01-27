292c631184c70e87a73f6095a836012bb98b4246
radenkovn <radenkovn@yahoo.com>
2016-01-28 00:54:04 +0200

    Initial Commit

---
+++ HolidayDestinations/HolidayDestinations.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HolidayDestinations.Web.Global" Language="C#" %>
---
+++ HolidayDestinations/HolidayDestinations.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HolidayDestinations.Web.Startup))]
+namespace HolidayDestinations.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
