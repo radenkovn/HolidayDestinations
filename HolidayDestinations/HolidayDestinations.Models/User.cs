ea6c3c9d8a28252437af463b18aa14ff664baa70
radenkovn <radenkovn@yahoo.com>
2016-01-28 02:01:39 +0200

    Added Reservations

--- HolidayDestinations/HolidayDestinations.Models/User.cs
+++ HolidayDestinations/HolidayDestinations.Models/User.cs
@@ -2,11 +2,21 @@
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Collections.Generic;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
     public class User : IdentityUser
     {
+        private ICollection<Reservation> reservations;
+
+        public User()
+        {
+            this.reservations = new HashSet<Reservation>();
+        }
+
+        public virtual ICollection<Reservation> Reservations { get { return this.reservations; } set { this.reservations = value; } }
+
         public ClaimsIdentity GenerateUserIdentity(UserManager<User> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
