7df22a2789286f97ad79dfe12d19c37604d6db3f
radenkovn <radenkovn@yahoo.com>
2016-01-28 03:07:56 +0200

    Added a Destination Service

---
+++ HolidayDestinations/HolidayDestinations.Services/Contracts/IDestinationsService.cs
@@ -0,0 +1,14 @@
+﻿namespace HolidayDestinations.Services.Contracts
+{
+    using HolidayDestinations.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IDestinationsService
+    {
+        IQueryable<Destination> GetLatest();
+    }
+}
---
+++ HolidayDestinations/HolidayDestinations.Services/DestinationsService.cs
@@ -0,0 +1,25 @@
+﻿namespace HolidayDestinations.Services
+{
+    using HolidayDestinations.Data.Repositories;
+    using HolidayDestinations.Models;
+    using HolidayDestinations.Services.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class DestinationsService: IDestinationsService
+    {
+        private IRepository<Destination> destinations;
+        public DestinationsService(IRepository<Destination> destinations)
+        {
+            this.destinations = destinations;
+        }
+        public IQueryable<Destination> GetLatest()
+        {
+            var timeToCompare = DateTime.Now;
+            return this.destinations.All().Where(x => x.Date > timeToCompare).OrderBy(x => x.Date - timeToCompare);
+        }
+    }
+}
