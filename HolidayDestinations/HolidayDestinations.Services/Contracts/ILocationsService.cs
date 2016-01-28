4ef96dc7602add0bdde355a1399638e6fd886632
radenkovn <radenkovn@yahoo.com>
2016-01-28 14:02:50 +0200

    Added Locations Service

---
+++ HolidayDestinations/HolidayDestinations.Services/Contracts/ILocationsService.cs
@@ -0,0 +1,10 @@
+﻿namespace HolidayDestinations.Services.Contracts
+{
+    using Models;
+    using System.Linq;
+
+    public interface ILocationsService
+    {
+        IQueryable<Location> GetAll();
+    }
+}
