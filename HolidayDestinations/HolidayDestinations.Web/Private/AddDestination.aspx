4e5118181a7174a70194873c2cb0266a29c694ce
radenkovn <radenkovn@yahoo.com>
2016-01-28 14:02:12 +0200

    Added Basic Add Destination Functionality

--- HolidayDestinations/HolidayDestinations.Web/Private/AddDestination.aspx
+++ HolidayDestinations/HolidayDestinations.Web/Private/AddDestination.aspx
@@ -1,3 +1,52 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddDestination.aspx.cs" Inherits="HolidayDestinations.Web.Private.AddDestination" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:FormView ID="fvAddDestination"
+        runat="server"
+        InsertMethod="fvAddDestination_InsertItem"
+        ItemType="HolidayDestinations.Models.Destination"
+        DefaultMode="Insert">
+        <InsertItemTemplate>
+            <h3>Title:
+               
+                <asp:TextBox Text="<%# BindItem.Title %>" ID="fvInsertTitle" runat="server" />
+            </h3>
+            <p>
+                <label class="col-md-12 text-info">
+                    Category:               
+                    <asp:DropDownList
+                        ID="ddlInsertCategory"
+                        runat="server"
+                        DataTextField="Name"
+                        DataValueField="Id"
+                        SelectMethod="ddlInsertCategory_GetData"
+                        SelectedValue="<%# BindItem.CategoryId %>"
+                        CssClass="form-control text-primary">
+                    </asp:DropDownList>
+
+                </label>
+            </p>
+
+            <p>
+                <label class="col-md-12 text-info">
+                    Location:               
+                    <asp:DropDownList
+                        ID="DropDownList1"
+                        runat="server"
+                        DataTextField="Name"
+                        DataValueField="Id"
+                        SelectMethod="ddlInsertLocation_GetData"
+                        SelectedValue="<%# BindItem.LocationId %>"
+                        CssClass="form-control text-primary">
+                    </asp:DropDownList>
+                </label>
+            </p>
+            <p>
+                Content:               
+                <asp:TextBox Text="<%# BindItem.Description %>" ID="tbDescription" runat="server" TextMode="MultiLine" Rows="8" />
+                <asp:FileUpload ID="imageUploadControl" runat="server" />
+            </p>
+            <asp:Button Text="Insert" CommandName="Insert" runat="server" CssClass="btn btn-success" />
+        </InsertItemTemplate>
+    </asp:FormView>
 </asp:Content>
--- HolidayDestinations/HolidayDestinations.Web/Private/AddDestination.aspx.cs
+++ HolidayDestinations/HolidayDestinations.Web/Private/AddDestination.aspx.cs
@@ -1,17 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace HolidayDestinations.Web.Private
+﻿namespace HolidayDestinations.Web.Private
 {
+    using System;
+    using System.Collections;
+    using System.Linq;
+
+    using Ninject;
+
+    using HolidayDestinations.Services.Contracts;
+    using Models;
+    using Microsoft.AspNet.Identity;
+    using System.Web.UI.WebControls;
+    using System.IO;
     public partial class AddDestination : System.Web.UI.Page
     {
+        [Inject]
+        public ILocationsService LocationsService { get; set; }
+
+        [Inject]
+        public ICategoriesService CategoriesService { get; set; }
+
+        [Inject]
+        public IDestinationsService DestinationsService { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public void fvAddDestination_InsertItem()
+        {
+            var newDestination = new Destination()
+            {
+                UserId = User.Identity.GetUserId(),
+                Date = DateTime.UtcNow.AddDays(3)
+            };
+
+            var fileUploader = this.fvAddDestination.FindControl("imageUploadControl") as FileUpload;
+            string filepath = string.Empty;
+            if (fileUploader.HasFile)
+            {
+                string filename = Path.GetFileName(fileUploader.FileName);
+                filepath = "/Images/" + filename;
+                fileUploader.SaveAs(Server.MapPath("~" + filepath));
+            }
+            filepath = "." + filepath;
+            newDestination.PhotoUrl = filepath;
+            TryUpdateModel(newDestination);
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    this.DestinationsService.Create(newDestination);
+                }
+                catch
+                {
+                    Response.Redirect("/Private/AddDestination");
+                }
+            }
+        }
+
+        public IQueryable<Category> ddlInsertCategory_GetData()
+        {
+            return this.CategoriesService.GetAll();
+        }
+
+        public IQueryable<Location> ddlInsertLocation_GetData()
+        {
+            return this.LocationsService.GetAll();
         }
     }
 }
