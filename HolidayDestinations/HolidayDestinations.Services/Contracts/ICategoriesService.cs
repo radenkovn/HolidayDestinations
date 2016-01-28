8e2cfacf840034692802eb65bf32da20d96ad190
radenkovn <radenkovn@yahoo.com>
2016-01-28 14:02:38 +0200

    Added Categories Service

---
+++ HolidayDestinations/HolidayDestinations.Services/Contracts/ICategoriesService.cs
@@ -0,0 +1,10 @@
+﻿namespace HolidayDestinations.Services.Contracts
+{
+    using Models;
+    using System.Linq;
+
+    public interface ICategoriesService
+    {
+        IQueryable<Category> GetAll();
+    }
+}
