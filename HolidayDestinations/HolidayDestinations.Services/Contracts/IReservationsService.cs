daa722ba88c89f5b190b2ac9139ee44341afe36a
radenkovn <radenkovn@yahoo.com>
2016-01-28 07:18:40 +0200

    Added Create Reservation Method

---
+++ HolidayDestinations/HolidayDestinations.Services/Contracts/IReservationsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidayDestinations.Services.Contracts
+{
+    public interface IReservationsService
+    {
+        void CreateReservation(string userId, int destinationId);
+    }
+}
