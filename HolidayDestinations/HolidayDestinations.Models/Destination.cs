58e4882d542cf71e924889e29411e3a4d055b410
radenkovn <radenkovn@yahoo.com>
2016-01-28 02:01:32 +0200

    Added destinations

---
+++ HolidayDestinations/HolidayDestinations.Models/Destination.cs
@@ -0,0 +1,27 @@
+﻿namespace HolidayDestinations.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Destination
+    {
+        private ICollection<Reservation> reservations;
+
+        public Destination()
+        {
+            this.reservations = new HashSet<Reservation>();
+        }
+
+        public int Id { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public DestinationType DestinationType { get; set; }
+
+        public virtual ICollection<Reservation> Reservations { get { return this.reservations; } set { this.reservations = value; } }
+    }
+}
